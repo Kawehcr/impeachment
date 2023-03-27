bc60ce3ff33f6d89a07947f19098f07684d1027a
kawe <kawehenriquedacostarodrigues@gmail.com>
2023-03-27 17:48:58 -0300

    [BUGFIX] Scripts

--- scripts/V0.0.1_CREATE_MODEL_IMPEACHMENT.sql
+++ scripts/V0.0.1_CREATE_MODEL_IMPEACHMENT.sql
@@ -1,6 +1,6 @@
 CREATE TABLE impeachments(
 	id INT NOT NULL,
     company_id INT NOT NULL,
-	name_impeachment VARCHAR(60) NULL,
-	description_impeachment TEXT NULL
-) CONSTRAINT pk_Impeachment primary key(id)
+	name_impeachment VARCHAR(60),
+	description_impeachment TEXT
+) CONSTRAINT pk_impeachments primary key(id)
